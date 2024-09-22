a856ea5694ff2fc60614d42ee0f5f0827ff9d19f
wyattlake <lakewyatt@gmail.com>
2024-09-22 02:06:16 -0400

    Added favicon and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,52 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { session } from '$lib/session';
-    import { goto } from '$app/navigation';
-    import { signOut } from 'firebase/auth';
-    import { auth } from '$lib/firebase.client';
-    import { onAuthStateChanged } from 'firebase/auth';
-   
-    import type { LayoutData } from './$types';
-    export let data: LayoutData;
-   
-    let loading: boolean = true;
-    let loggedIn: boolean = false;
-   
-    session.subscribe((cur: any) => {
-     loading = cur?.loading;
-     loggedIn = cur?.loggedIn;
-    });
-   
-    onMount(async () => {
-     const user: any = await new Promise((resolve) => {
-        onAuthStateChanged(auth, (user) => resolve(user ? user : false));
-        });
-   
-     const loggedIn = !!user && user?.emailVerified;
-     session.update((cur: any) => {
-      loading = false;
-      return {
-       ...cur,
-       user,
-       loggedIn,
-       loading: false
-      };
-     });
-   
-     if (loggedIn) {
-      goto('/');
-     }
-    });
-   
-   </script>
+	import { onMount } from 'svelte';
+	import { session } from '$lib/session';
+	import { goto } from '$app/navigation';
+	import { signOut } from 'firebase/auth';
+	import { auth } from '$lib/firebase.client';
+	import { onAuthStateChanged } from 'firebase/auth';
+
+	import type { LayoutData } from './$types';
+	export let data: LayoutData;
+
+	let loading: boolean = true;
+	let loggedIn: boolean = false;
+
+	session.subscribe((cur: any) => {
+		loading = cur?.loading;
+		loggedIn = cur?.loggedIn;
+	});
+
+	onMount(async () => {
+		const user: any = await new Promise((resolve) => {
+			onAuthStateChanged(auth, (user) => resolve(user ? user : false));
+		});
+
+		const loggedIn = !!user && user?.emailVerified;
+		session.update((cur: any) => {
+			loading = false;
+			return {
+				...cur,
+				user,
+				loggedIn,
+				loading: false
+			};
+		});
+
+		if (loggedIn) {
+			goto('/');
+		}
+	});
+</script>
 
 {#if loading}
-<div>Loading...</div>
+	<div>Loading...</div>
 {:else}
- <div>
-  <slot />
- </div>
+	<div>
+		<slot />
+	</div>
 {/if}
+
+<svelte:head>
+	<title>Metrics</title>
+</svelte:head>
