638cf9f78b799d1e891ab5918fbb5ef131efe717
Evelyn Li <evelynli@brb-04193.apn.wlan.private.upenn.edu>
2024-09-21 21:41:41 -0400

    home page

---
+++ src/icons/titleGraphic.svelte
@@ -0,0 +1,15 @@
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1198.287" height="474.674" viewBox="0 0 1198.287 474.674">
+    <defs>
+      <linearGradient id="linear-gradient" x1="0.5" x2="0.5" y2="1" gradientUnits="objectBoundingBox">
+        <stop offset="0" stop-color="#d8fddc"/>
+        <stop offset="0.424" stop-color="#ecfeee"/>
+        <stop offset="1" stop-color="#f7fff8"/>
+      </linearGradient>
+    </defs>
+    <g id="Group_33" data-name="Group 33" transform="translate(-369.992 -662.646)">
+      <path id="Path_12" data-name="Path 12" d="M3786.7,903.26,3527.3,1022.292l-215.686-25.979-80.308,89.045-319.114,135.773,908.911,145.1Z" transform="translate(-2255.552 -231.884)" stroke="#95e398" stroke-width="5" fill="url(#linear-gradient)"/>
+      <path id="Path_13" data-name="Path 13" d="M2987.271,1228.526l231.709-235.147,111.079,63.455,199.4-190.142,293.676,153.867" transform="translate(-2260.72 -201)" fill="none" stroke="#f3a0f7" stroke-width="5"/>
+      <path id="Path_14" data-name="Path 14" d="M3316.218,1325.284l101.689-122.665L3614.94,1091.9l282.743,25.243" transform="translate(-2342.596 -257.054)" fill="none" stroke="#54bdff" stroke-width="5"/>
+      <path id="Path_15" data-name="Path 15" d="M3702.9,1139.1l-299.852-181.88L3221.276,1139.1l-342.6-51.149-364.355,227.532" transform="translate(-2143 -223.534)" fill="none" stroke="#dcf086" stroke-width="5"/>
+    </g>
+  </svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,176 @@
+<script>
+  import Typewriter from 'svelte-typewriter';
+  import Logo from '../icons/logo.svelte';
+  import Background from '../icons/background.svelte';
+  import TitleGraphic from '../icons/titleGraphic.svelte';
+</script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+<div class="background-container">
+  <Background />
+
+</div>
+
+<div class="background-graph-container">
+  <TitleGraphic />
+</div>
+
+<div class="content-container">
+  <header class="mb-2">
+    <div class="logo">
+      <Logo scale={0.38} />
+    </div>
+  </header>
+  <main class="flex-grow flex items-center">
+    <div class="text-container">
+      <h1>Building 
+        <span class="gradient-text">data analytics</span> 
+        solutions for <br>
+        <span class="typewriter-container">
+          <Typewriter mode="loop">
+            <h1 class="gradient-text">small businesses</h1>
+            <h1 class="gradient-text">nonprofits</h1>
+            <h1 class="gradient-text">innovators</h1>
+          </Typewriter>
+        </span> 
+      </h1>
+    <h2>We utilize web scraping and super fast AI technology to create competitive
+      recommendations for your business
+    </h2>
+    </div>
+    <div class="button-container" style="margin-top: 50px">
+      <a href="/login" class="btn btn-secondary">Login</a>
+      <a href="/signup" class="btn btn-primary">Sign Up</a>
+    </div>
+  </main>
+</div>
 
 <style>
   :global(body) {
     font-family: Arial, sans-serif;
-    line-height: 1.6;
+    line-height: 1.2;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+  }
+
+  .btn {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem 1rem;
+    font-size: 1.4rem;
+    font-weight: 600;
+    text-decoration: none;
+    border-radius: 1rem;
+    transition: background-color 0.3s ease;
+    width: 135px;
+    height: 40px;
+  }
+
+  .btn:focus {
+    outline: none;
+    box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5);
+  }
+
+  .btn-primary {
+    background-color: #4ecdc4;
+    color: white;
+    border: 0.6px solid #4ecdc4;
+  }
+
+  .btn-primary:hover {
+    background-color: #3182ce;
+  }
+
+  .btn-secondary {
+    background-color: #edf4fb;
+    color: #000000;
+    width: 100px;
+    border: 0.6px solid #808080;
+  }
+
+  .btn-secondary:hover {
+    background-color: #e2e8f0;
+  }
+
+  .background-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+
+  .background-container :global(svg) {
+    width: 130vw;
+    height: 160vh;
+    transform: translate(-19%, -24%) rotate(0.51turn);
+    object-fit: cover;
+  }
+
+  .background-graph-container {
+    position: absolute;
+    bottom: -150px;
+    right: -70px;
+  }
+
+  .content-container {
+    position: relative;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    padding: 4rem;
+  }
+
+  .text-container {
+    max-width: 1000px;
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .button-container {
+    display: flex;
+    gap: 1rem;
+    margin-top: 1rem;
+  }
+
+  .typewriter-container {
+    --cursor-width: 1px;
+    display: flex;
+  }
+
+  h1 {
+    font-size: 3.5rem;
+    margin: 0;
+  }
+
+  h2 {
+    font-size: 1.6rem;
+    font-weight: normal;
+  }
+
+  .gradient-text {
+    display: inline-block;
+    background: linear-gradient(180deg, #99ff66, #4ecdc4, #45b7d1);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 200% 200%;
+  }
+
+  @keyframes gradient {
+    0% { background-position: 0% 50%; }
+    50% { background-position: 100% 50%; }
+    100% { background-position: 0% 50%; }
+  }
+
+  @media (max-width: 400px) {
+    h1 {
+      font-size: 2rem;
+    }
   }
 </style>
-
-
-<script>
-    import Typewriter from 'svelte-typewriter';
-  </script>
-  
-  <main class="container mx-auto px-4 py-8">
-    <header class="flex justify-between items-center mb-2">
-        <div class='logo'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" viewBox="0 0 400 100">
-                <g id="Group_34" data-name="Group 34" transform="translate(-140 -300)">
-                  <text id="mtrx" transform="translate(203 356)" font-size="80" font-family="Inter-SemiBold, Inter" font-weight="600"><tspan x="0" y="0">mtrx</tspan></text>
-                  <text id="_" data-name="[" transform="translate(140 392)" fill="#95e398" font-size="200" font-family="Inter-Light, Inter" font-weight="300"><tspan x="0" y="0">[</tspan></text>
-                  <text id="_2" data-name="]" transform="translate(383 392)" fill="#95e398" font-size="200" font-family="Inter-Light, Inter" font-weight="300"><tspan x="0" y="0">]</tspan></text>
-                </g>
-        </div>
-      </header>    
-    <Typewriter mode="loop" cursor={false}>
-      <h2 class="text-2xl mb-4">Building data analytics solutions for 
-        <span class="text-blue-500">small businesses</span>
-      </h2>
-      <h2 class="text-2xl mb-4">Building data analytics solutions for
-        <span class="text-green-500">nonprofits</span>
-      </h2>
-      <h2 class="text-2xl mb-4">Building data analytics solutions for
-        <span class="text-purple-500">innovators</span>
-      </h2>
-    </Typewriter>
-    
-    <p class="mt-8">
-      This is a simple home page with a typing animation. The text above will 
-      continuously loop through different phrases.
-    </p>
-</main>
