0796422d713bd5ca2553e5eeb6e8025a8a7b81e1
wyattlake <lakewyatt@gmail.com>
2024-09-21 22:19:08 -0400

    home page:

---
+++ src/icons/logo.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let scale = 1.0;
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={313 * scale}
+	height={242 * scale}
+	viewBox="0 0 313 242"
+>
+	<g id="logo" data-name="logo" transform="translate(-140 -198)">
+		<text
+			id="mtrx"
+			transform="translate(203 356)"
+			font-size="80"
+			font-family="Inter-SemiBold, Inter"
+			font-weight="600"><tspan x="0" y="0">mtrx</tspan></text
+		>
+		<text
+			id="_"
+			data-name="["
+			transform="translate(140 392)"
+			fill="#95e398"
+			font-size="200"
+			font-family="Inter-Light, Inter"
+			font-weight="300"><tspan x="0" y="0">[</tspan></text
+		>
+		<text
+			id="_2"
+			data-name="]"
+			transform="translate(383 392)"
+			fill="#95e398"
+			font-size="200"
+			font-family="Inter-Light, Inter"
+			font-weight="300"><tspan x="0" y="0">]</tspan></text
+		>
+	</g>
+</svg>
